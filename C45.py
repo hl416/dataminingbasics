8d3c016660aec5a101559c182ba5aea29bb7a09e
Hao Liu <liuhao.buaa@gmail.com>
2015-03-08 14:02:36 +0800

    add C4.5

---
+++ C45.py
@@ -0,0 +1,68 @@
+import math
+
+dict_tree = {}
+
+def calc_entory(lst_data):
+	dict_label_count = {}
+	for d in lst_data:
+		label = d[-1]
+		dict_label_count.setdefault(label,0)
+		dict_label_count[label] += 1
+	lst_prob = [dict_label_count[l]/float(len(lst_data)) for l in dict_label_count]
+	print lst_prob
+	return -sum([p * math.log(p,2) for p in lst_prob])
+
+def split_data(lst_data,feature_idx,feature_value):
+	return [d[:feature_idx]+d[feature_idx+1:] for d in lst_data if d[feature_idx] == feature_value]		
+
+def select_feature(lst_data):
+	base_entroy = calc_entory(lst_data)
+	max_gain = 0.0
+	best_feature_idx = 0
+	for i in range(0,len(lst_data[0])-1):
+		cur_entropy = 0.0
+		for value in set([d[i] for d in lst_data]):
+			sub_data = split_data(lst_data,i,value)
+			prob = len(sub_data)/float(len(lst_data))
+			cur_entropy += calc_entory(sub_data) * prob
+		info_gain = base_entroy - cur_entropy
+		if  info_gain > max_gain:
+			max_gain = info_gain 
+			best_feature_idx = i
+	return best_feature_idx
+
+def major_label(lst_labels):
+	dict_label_cnt = {}
+	for l in lst_labels:
+		dict_label_count.setdefault(l,0)
+		dict_label_count[l] += 1
+	max_cnt = 0
+	max_label = ''
+	for l in dict_label_count:
+		if dict_label_count[l] > max_cnt:
+			max_cnt = dict_label_count[l]
+			max_label = l
+	return max_label
+
+def build_tree(lst_data):
+	print lst_data
+	lst_labels = [d[-1] for d in lst_data]
+	if (lst_labels.count(lst_labels[0]) == len(lst_labels)):#only one catogory now
+		print "only one category"
+		return lst_labels[0]
+	elif len(lst_data[0]) == 1: #all features have been used
+		print "no features left"
+		return major_label(lst_labels)
+	dict_tree_tmp = {}
+	best_feature_idx = select_feature(lst_data)
+	print "feature selected: %s" % str(set([d[best_feature_idx] for d in lst_data]))
+	for value in set([d[best_feature_idx] for d in lst_data]):
+		dict_tree_tmp[value] = build_tree(split_data(lst_data,best_feature_idx,value))
+	return dict_tree_tmp
+
+
+if __name__ == "__main__":
+	lst_data = [d.strip('\n').split(',') for d in open("C4.5.data")]
+	print calc_entory(lst_data)
+	print build_tree(lst_data)
+	
